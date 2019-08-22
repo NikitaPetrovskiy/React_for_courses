d1169233131b8fe734e076f79afc5b9702308eca
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-08-22 16:21:14 +0300

    Did refactoring. Moved Button's constants to component, renamed some properties, changed and added logic operator '&&'

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../index.css';
 
+export const KINDS = Object.freeze({
+    INFO: 'info', SUCCES: 'success', WARNING: 'warning', DANGER: "danger", DEFAULT: 'default'
+});
+//text for button
+export const TEXT_FOR_BUTTON = Object.freeze({
+    HOME: 'HOME', MENU: 'MENU', CLOSE: 'CLOSE', FOLDER: 'FOLDER'
+})
+
 export const Button = (props) => (
-    <button className={"btn " + props.classBtn} style={{borderRadius: props.radius}}>
-        {props.loader ? <i className={"fa fa-" + props.loader + " fa-spin"}></i> : null}
-        {props.icon ? <i className={"fa fa-" + props.icon}></i> : null}
+    <button className={props.kind ? "btn " + props.kind : "btn default"} style={{borderRadius: props.radius}}>
+        {props.loader && <i className={"fa fa-" + props.loader + " fa-spin"} />}
+        {props.icon && <i className={"fa fa-" + props.icon} />}
         {props.text}
     </button>
 );
--- src/index.js
+++ src/index.js
@@ -3,40 +3,31 @@
 import React, { Component } from 'react';
 import * as serviceWorker from './serviceWorker';
 import { render } from 'react-dom';
-import {Button} from './Components/Button';
+import {Button, KINDS, TEXT_FOR_BUTTON} from './Components/Button';
 // import Icon from './Components/Icon';
 
-//classes for button
-const INFO = 'info';
-const SUCCES = 'success';
-const WARNING = 'warning';
-const DANGER = "danger";
-const DEFAULT = 'default';
 //classes for loader icons
-const REFRESH = 'refresh';
-const SPINNER = 'spinner';
-const CIRCLE = 'circle-o-notch';
+const ICONS_LOAD = Object.freeze({
+    REFRESH:'refresh', SPINNER: 'spinner', CIRCLE: 'circle-o-notch'
+})
 //classes for image icon
-const HOME_ICON = 'home';
-const TRASH_ICON = 'trash';
-const CLOSE_ICON = 'close';
-const FOLDER_ICON = 'folder';
-//text for button
-const HOME = 'HOME';
-const MENU = 'MENU';
-const CLOSE = 'CLOSE';
-const FOLDER = 'FOLDER';
+const ICONS_IMG = Object.freeze({
+    HOME_ICON: 'home', TRASH_ICON: 'trash', CLOSE_ICON: 'close', FOLDER_ICON: 'folder'
+})
 
     const App = (
         <div>
-            <Button classBtn={INFO} radius={10} text={HOME} loader={SPINNER} icon={HOME_ICON}>
+            <Button kind={KINDS.INFO} radius={10} text={TEXT_FOR_BUTTON.HOME}
+                    loader={ICONS_LOAD.SPINNER} icon={ICONS_IMG.HOME_ICON}>
                 {/*<Icon loader={SPINNER} />*/}
                 {/*<Icon icon={HOME_ICON} />*/}
             </Button>
-            <Button classBtn={SUCCES} radius={5} text={MENU}/>
-            <Button classBtn={WARNING} icon={TRASH_ICON} />
-            <Button classBtn={DANGER} radius={15} icon={CLOSE_ICON} loader={CIRCLE} text={CLOSE} />
-            <Button classBtn={DEFAULT} icon={FOLDER_ICON} loader={REFRESH} radius={50} text={FOLDER} />
+            <Button kind={KINDS.SUCCES} radius={5} text={TEXT_FOR_BUTTON.MENU}/>
+            <Button kind={KINDS.WARNING} icon={ICONS_IMG.TRASH_ICON} />
+            <Button kind={KINDS.DANGER} radius={15} icon={ICONS_IMG.CLOSE_ICON}
+                    loader={ICONS_LOAD.CIRCLE} text={TEXT_FOR_BUTTON.CLOSE} />
+            <Button kind={KINDS.DEFAULT} icon={ICONS_IMG.FOLDER_ICON} loader={ICONS_LOAD.REFRESH}
+                    radius={50} text={TEXT_FOR_BUTTON.FOLDER} />
         </div>
     );
 
